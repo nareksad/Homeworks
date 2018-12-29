7132daec31ab045817c030a53d1ad869d9780a02
nareksad <narek.sadoyan92@gmail.com>
2018-12-29 17:21:49 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //int number = 5;
+        //double number2 = 1.2;
+        //float number3 = 3;
+        //byte bit = 1;
+        //short ah = 2;
+        //System.out.println(number);
+        //System.out.println(number2);
+        //System.out.println(number3);
+        //System.out.println(bit);
+        //System.out.println(ah);
+
+        //int result =  calc(14,23,4,2);
+        //System.out.println(result);
+        // boolean result = ham(1,8);
+        // System.out.println(result);
+//String DE = Jog(-5);
+//System.out.println(DE);
+        // }
+      //  boolean ardyunq = siko(0);
+        //System.out.println(ardyunq);
+
+
+        //public static int calc(int a, int b, int c, int d){
+
+        //  return a * (b + (c / d));
+
+
+        //}
+        //public static boolean ham(int a,int b){
+        // int c = a + b;
+        //boolean res;
+        //if (c <= 20 && c >= 10){
+        //  res = true;
+
+
+        //}
+        //else {
+        //  res = false;
+//        }
+
+
+        //return res;
+        //  }
+
+
+//}
+//public static java.lang.String Jog(int a) {
+        //  String plmn = "PLUS";
+        //String mlpn = "MINUS";
+        //if (a < 0) {
+        //  return mlpn;
+        //} else {
+        //  return plmn;
+        //}
+//    }
+//public static boolean siko(int a){
+  //  boolean resultat;
+    //if (a < 0){
+      //  resultat = true;
+    //}
+    //else {
+      //  resultat = false;
+    //}
+    //return resultat;
+//        }
+
+
+  // Pox("Bro");
+}
+// public static void Pox(String Anun){
+  //      System.out.println("Hello" + " " + Anun);
+ //}
+}
